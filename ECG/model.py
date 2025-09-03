6947c6a300fa7ecf62373f03dd146865330ed8e2
summer <summer35174@gmail.com>
2025-09-03 17:24:59 +0800

    model update

--- ECG/model.py
+++ ECG/model.py
@@ -13,19 +13,24 @@ class CNN1D(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=2, stride=2)
         )
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, padding=0, stride=2),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool1d(1)
+        )
         self.fc1 = nn.Sequential(
             nn.Dropout(0.5),
-            nn.Flatten(),
-            nn.Linear(in_features=32*349, out_features=512),
+            nn.Linear(in_features=64, out_features=32),
             nn.ReLU(inplace=True),
             nn.Dropout(0.3),
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(in_features=512, out_features=num_classes),
+            nn.Linear(in_features=32, out_features=num_classes),
         )
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
+        x = self.conv3(x)
         x = self.fc1(x)
         x = self.fc2(x)
         return x
