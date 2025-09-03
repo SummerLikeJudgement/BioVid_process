de922725666ec9fe7b3ddb59c4cad573abcf29fc
summer <summer35174@gmail.com>
2025-09-03 17:14:41 +0800

    model update

--- ECG/model.py
+++ ECG/model.py
@@ -4,20 +4,21 @@ class CNN1D(nn.Module):
     def __init__(self, num_classes=5):
         super(CNN1D, self).__init__()
         self.conv1 = nn.Sequential(
-            nn.Conv1d(in_channels=1, out_channels=32, kernel_size=5, padding=0, stride=2),
+            nn.Conv1d(in_channels=1, out_channels=16, kernel_size=5, padding=0, stride=2),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=2, stride=2)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=5, padding=0, stride=1),
+            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=5, padding=0, stride=1),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=2, stride=2)
         )
         self.fc1 = nn.Sequential(
+            nn.Dropout(0.5),
             nn.Flatten(),
             nn.Linear(in_features=64*349, out_features=512),
             nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
         )
         self.fc2 = nn.Sequential(
             nn.Linear(in_features=512, out_features=num_classes),
